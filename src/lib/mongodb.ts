543747d9bf15c607db56d0cc4b5faeef76489e96
enzowagaokada <enzookada37@gmail.com>
2025-09-14 18:29:55 -0400

    9/14/2025 set up src files, mongodb set up, and authentification route

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,5 +1,47 @@
 import mongoose from 'mongoose';
-const MONGODB_URI = process.env.MONGODB_URI;
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
 if (!MONGODB_URI) {
-    throw new Error('Please define MONGODB_URI enviornment variable in .env.local');
+    throw new Error('Please define MONGODB_URI environment variable in .env.local');
 }
+// add mongooseCache property
+declare global {
+    var mongooseCache: {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+    };
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+    cached = global.mongooseCache = {conn: null, promise: null};
+}
+
+async function dbConnect() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        };
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+            return mongoose;
+        });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (error) {
+        cached.promise = null;
+        throw error;
+    }
+
+    return cached.conn;
+}
+
+export default dbConnect;
