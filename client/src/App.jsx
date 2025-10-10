8430c3e21377374b71d320ad28ebc1dcb4a7a1b1
Waga Okada, Enzo <130874071+enzowagaokada@users.noreply.github.com>
2025-10-09 23:55:11 -0400

    remove boxes and simplify home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -9,6 +7,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [isLoading, setIsLoading] = useState(true)
+  
   useEffect(() => {
     fetch(`${API_BASE_URL}/api/is-authenticated/`, {
       credentials: 'include',
@@ -34,38 +33,41 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <header>
-        <h1>GenreSort for Spotify</h1>
-        <p className="subtitle">Sort your saved playlists by genre.</p>
-      </header>
+    <div>
+      {/* Home Page */}
+      <div className="container">
+        <header>
+          <h1>Genre Sort for Spotify</h1>
+          <p className="subtitle">Sort your saved playlists by genre.</p>
+        </header>
 
-      <main className="card">
-        {isAuthenticated ? (
-          <div>
-            <h2>You are logged in!</h2>
-            <p>Ready to sort your playlists? Click the button below to get started.</p>
-            <button onClick={handleFetchPlaylists}>
-              Fetch My Playlists
-            </button>
-          </div>
-        ) : (
-          <div>
-            <h2>Welcome!</h2>
-            <p>Please log in with your Spotify account to continue.</p>
-            <a href={`${API_BASE_URL}/api/login/`}>
-              <button>
-                Login with Spotify
+        <main className="card">
+          {isAuthenticated ? (
+            <div>
+              <h2>You are logged in!</h2>
+              <p>Ready to sort your playlists? Click the button below to get started.</p>
+              <button onClick={handleFetchPlaylists}>
+                Fetch My Playlists
               </button>
-            </a>
-          </div>
-        )}
-      </main>
-      <footer>
-        <p>
-          Powered by the Spotify Web API.
-        </p>
-      </footer>
+            </div>
+          ) : (
+            <div>
+              <h2>Welcome!</h2>
+              <p>Please log in with your Spotify account to continue.</p>
+              <a href={`${API_BASE_URL}/api/login/`}>
+                <button>
+                  Login with Spotify
+                </button>
+              </a>
+            </div>
+          )}
+        </main>
+        <footer>
+          <p>
+            Powered by the Spotify Web API.
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
